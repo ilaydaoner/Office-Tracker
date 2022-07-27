f3f76c93425c566ff5f5169b4bedb98bf0092a70
İlayda Öner <ilayda.oner@mobven.com>
2022-07-27 20:36:07 +0300

    Login and register page created

--- Office Tracker/ViewController.swift
+++ Office Tracker/ViewController.swift
@@ -8,9 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var doyouhaveLabel: UILabel!
+    @IBOutlet weak var signinButton: UIButton!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        signinButton.layer.cornerRadius = 15
+        registerButton.layer.cornerRadius = 15
+        emailLabel.textColor = UIColor(named: "purpletoDark")
+        signinButton.setTitleColor(UIColor(named: "whitetoDark"), for: .normal)
+        registerButton.setTitleColor(UIColor(named: "whitetoDark"), for: .normal)
+        
+        passwordLabel.textColor = UIColor(named: "purpletoDark")
+        signinButton.backgroundColor = UIColor(named: "purpletoDark")
+        registerButton.backgroundColor = UIColor(named: "purpletoDark")
+        
+        
+        
         // Do any additional setup after loading the view.
     
 let showVersion = (Bundle.main.infoDictionary?["SHOW_VERSION"] as? String) == "YES"
@@ -19,6 +40,8 @@ let showVersion = (Bundle.main.infoDictionary?["SHOW_VERSION"] as? String) == "Y
     }
 }
     
+    @IBAction func signinTap(_ sender: UIButton) {
+    }
 }
 
 
